3ce3035a82ecf52042afe0279cec32aede1e72d9
kylegilde <kylegilde@gmail.com>
2018-12-15 21:47:14 -0600

    12/15

--- Final Project/resize-data.py
+++ Final Project/resize-data.py
@@ -1,7 +1,15 @@
 import pandas as pd
-reviews = pd.read_json('D:\Documents\yelp_dataset\yelp_academic_dataset_review.json', lines=True, chunksize=10000)
+
+# read json using chunks
+reviews = pd.read_json('D:\Documents\yelp_dataset\yelp_academic_dataset_review.json',
+                       lines=True,
+                       chunksize=10000)
+# filter & concatenate the chunks
 useful_reviews_df = pd.concat(reviews, ignore_index=True)\
-    .query("useful > 5 and date > '2016-01-01'")[['stars', 'text']]
+    .query("useful > 10 and date > '2016-01-01'")[['stars', 'text', 'useful']]
+# view dims
+print(useful_reviews_df.shape)
+# save reviews
 useful_reviews_df.to_csv('D:/documents/yelp_reviews.csv')
-useful_reviews_df.shape
-#useful_reviews = reviews[]
+
+
